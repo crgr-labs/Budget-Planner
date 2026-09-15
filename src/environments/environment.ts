3fa3b20cde7532a277996b28d239d95c30137c10
crgr-labs <crgr.labs@gmail.com>
2026-09-15 11:40:44 +0800

    chore: clean production environment default template

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  googleSheetsUrl: 'https://script.google.com/macros/s/AKfycbwEoC7gfYgAP8BXiyZtlS_QNyrGMEKX9tqkzhXeMCJJXSqrbUFsdUGBNF6RWgaEe9rq/exec?token=budget-planner-private-92sadf31s81sa2a255',
+  googleSheetsUrl: '',
 };
