f9ad9dee19f5c0f12b0384447f19c733c2338e90
crgr-labs <crgr.labs@gmail.com>
2026-09-15 11:40:44 +0800

    chore: clean production environment default template

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  googleSheetsUrl: 'https://script.google.com/macros/s/REDACTED-DEPLOYMENT-ID/exec?token=REDACTED-ROTATED-TOKEN',
+  googleSheetsUrl: '',
 };
